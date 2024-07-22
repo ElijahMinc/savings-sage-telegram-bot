6f533f72de2b28b3ef13c9f4f81959fe2d6e593e
Ilya Protsenko <ilya.protsenko@nixsolutions.com>
2024-07-22 18:29:51 +0100

    Added typeRoots, deleted unsuless code

--- src/commands/start.command.ts
+++ src/commands/start.command.ts
@@ -2,7 +2,6 @@ import { Telegraf } from "telegraf";
 import { IBotContext } from "@context/context.interface";
 import { Command } from "./command.class";
 import { COMMAND_NAMES } from "@/constants";
-import cron from "node-cron";
 import { getTimezone } from "@/helpers/getTimezone.helper";
 
 export class StartCommand extends Command {
@@ -14,25 +13,6 @@ export class StartCommand extends Command {
     this.bot.start(async (ctx) => {
       const timezone = await getTimezone();
       ctx.session.timezone = timezone;
-
-      // cron.schedule(
-      //   "* * * * *",
-      //   async () => {
-      //     console.log("Time to download the file!");
-
-      //     try {
-      //       // await downloadFile(fileUrl, localFilePath);
-      //       ctx.reply("Please, install file with your data");
-      //       console.log("File downloaded successfully");
-      //     } catch (error) {
-      //       console.error("Error downloading the file:", error);
-      //     }
-      //   },
-      //   {
-      //     scheduled: true,
-      //     timezone: timezone, // Замените на вашу временную зону, например, 'America/New_York'
-      //   }
-      // );
 
       ctx.reply(
         `Hello! Please choose primary tag first using /${COMMAND_NAMES.TAGS} command`
